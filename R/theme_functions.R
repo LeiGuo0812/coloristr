d92998e8ec39ed8e68c8ef93f8c09b8c01163e75
leiguo0812 <leiguo0812@gmail.com>
2025-01-03 15:28:51 +0800

    fix bold_fonts documents

--- R/theme_functions.R
+++ R/theme_functions.R
@@ -3,10 +3,16 @@
 #'
 #' @param face fontface for all text
 #' @param title_size_ratio relative size to original of all titles (plots, axis, legends, strips)
-#' @param legend_face fontface for legend text
-#'
+#' @param legend_text_face fontface for legend text
+#' @importFrom ggplot2 theme element_text rel
 #' @export
-#'
+#' @examples
+#' library(ggplot2)
+#' mtcars |> 
+#'   ggplot(aes(x = disp, y = mpg)) +
+#'   geom_point() +
+#'   bold_fonts()
+
 bold_fonts <- function(face = 'bold', title_size_ratio = 1.15, legend_text_face = 'plain'){
   theme(
     text = element_text(face = 'bold'),
